5f0c46b8e6e846e4fa67e8608ce3b0130b957388
jander.nunes <jander.nunes@conecthus.org.br>
2024-11-28 18:21:26 -0400

    first commit

---
+++ api/src/controllers/GoogleSignInController.ts
@@ -0,0 +1,57 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { GoogleApis } from "../services/GoogleApis";
+import { primaClient } from "../lib/prismaClient";
+
+const schema = z.object({
+  code: z.string().min(1),
+});
+
+export class GoogleSignInController {
+  static handle = async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const result = schema.safeParse(request.body);
+
+      if (!result.success) {
+        return reply.status(400).send({ error: result.error.issues });
+      }
+
+      const { code } = result.data;
+
+      const googleAccessToken = await GoogleApis.getAccessToken({
+        code,
+        redirectUri: "http://localhost:5173/callbacks/google",
+      });
+
+      const { verifiedEmail, ...userInfo } = await GoogleApis.getUserInfo(
+        googleAccessToken
+      );
+
+      await GoogleApis.revokeAccessToken(googleAccessToken);
+
+      if (!verifiedEmail) {
+        return reply.status(401).send({
+          error: "Google account is not verified.",
+        });
+      }
+
+      const user = await primaClient.user.upsert({
+        where: {
+          email: userInfo.email,
+        },
+        create: {
+          ...userInfo,
+        },
+        update: {
+          googleId: userInfo.googleId,
+        },
+      });
+
+      const accessToken = await reply.jwtSign({ sub: user.id });
+
+      return reply.code(200).send({ accessToken });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
